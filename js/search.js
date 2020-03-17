a124e3f8a8922813cb283a98693aa0036ae9364f
slvayf <slvayf@qq.com>
2020-03-17 19:59:07 +0800

    更丰富的搜索引擎

--- js/search.js
+++ js/search.js
@@ -1,13 +1,22 @@
 // 定义默认搜索引擎
 var defaultSearchEngines = 'https://www.baidu.com/s?wd=';
 
-// 自定义搜索工具箱，一条完整的搜索引擎或站点包括：标签[target]、标签对应网址或搜索引擎[url]
+// 自定义搜索工具箱，一条完整的搜索引擎或站点包括：标签[target]、标签对应的网址或可识别搜索参数搜索引擎链接[url]
 var data = [
-    { target: '谷歌', url: "'https://www.google.com/search?q='"},
-    { target: '豆瓣', url: "'https://www.douban.com/search?q='"},
-	{ target: '知乎', url: "'https://www.zhihu.com/search?type=content&q='"},
-	{ target: '腾讯视频', url: "'https://v.qq.com/x/search/?q='"},
-    { target: '哔哩哔哩', url: "'https://search.bilibili.com/all?keyword='"},
+    { target: '谷歌', url: "'https://www.google.com/search?q='" },
+	{ target: '必应', url: "'https://cn.bing.com/search?q='" },
+	{ target: '搜狗', url: "'https://www.sogou.com/web?query='" },
+	{ target: '360', url: "'https://www.so.com/s?q='" },
+	{ target: '百度百科', url: "'https://baike.baidu.com/item/'" },
+	{ target: '维基百科', url: "'https://zh.wikipedia.org/wiki/'" },
+	{ target: '京东', url: "'https://search.jd.com/Search?enc=utf-8&keyword='" },
+	{ target: '淘宝', url: "'https://s.taobao.com/search?q='" },
+    { target: '豆瓣', url: "'https://www.douban.com/search?q='" },
+	{ target: '知乎', url: "'https://www.zhihu.com/search?type=content&q='" },
+	{ target: '百度翻译', url: "'https://fanyi.baidu.com/#en/zh/'" },
+	{ target: '谷歌翻译', url: "'https://translate.google.cn/#view=home&op=translate&sl=auto&tl=zh-CN&text='" },
+	{ target: '腾讯视频', url: "'https://v.qq.com/x/search/?q='" },
+    { target: '哔哩哔哩', url: "'https://search.bilibili.com/all?keyword='" },
 ]
 
 // 核心搜索函数
